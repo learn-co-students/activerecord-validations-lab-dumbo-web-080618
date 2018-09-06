1314cc3249f2c24f185180d7c2736d3ac6e94819
github username <github email address>
2018-09-06 10:38:34 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,8 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "The Category must be either 'Fiction' or 'Non-Fiction'."}
   validate :click_baity
 
-  #regular expression used to check if the title is click bait-y
-  
   def click_baity
+    #regular expression used to check if the title is click bait-y
   reg = %r([\w\W]*Won't Believe [\w\W]*|[\w\W]*Secret[\w\W]*|[\w\W]*Top [d]*[\w\W]*|[\w\W]*Guess[\w\W]*)
     if title.present? && title.match(reg).nil?
       errors.add(:click_baity, "Title is not click-baity enough")
